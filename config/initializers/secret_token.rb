6008842f04227a70659c62aaa6073b8ced0400cc
akiii <akiii@sfc.wide.ad.jp>
2012-07-20 02:08:29 +0900

    first commi.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovoxServer::Application.config.secret_token = '1ce80b5a36b4a46aeace2b70b8bb93bf34bc5080d9641ebf995e7b0d4394a8f656c96f4cb0141927fdd2be9e896b918dc29af3edb45471febe6d0ddc0630fdf7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MovoxServer::Application.config.session_store :cookie_store, key: '_movox_server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MovoxServer::Application.config.session_store :active_record_store
